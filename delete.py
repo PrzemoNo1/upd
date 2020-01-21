84eb3d3695d75865af748e9d6323ff3c3cf1657e
PrzemoNo1 <37351230+PrzemoNo1@users.noreply.github.com>
2020-01-21 19:20:31 +0100

    Podstawowa aplikacja

---
+++ delete.py
@@ -0,0 +1,27 @@
+import mysql.connector
+from mysql.connector import errorcode
+from config import config_data
+
+# Construct connection string
+try:
+   conn = mysql.connector.connect(**config_data)
+   print("Connection established.")
+except mysql.connector.Error as err:
+  if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+    print("Something is wrong with the user name or password.")
+  elif err.errno == errorcode.ER_BAD_DB_ERROR:
+    print("Database does not exist.")
+  else:
+    print(err)
+else:
+  cursor = conn.cursor()
+
+  # Delete a data row in the table
+  cursor.execute("DELETE FROM inventory WHERE name=%(param1)s;", {'param1':"orange"})
+  print("Deleted",cursor.rowcount,"row(s) of data.")
+
+  # Cleanup
+  conn.commit()
+  cursor.close()
+  conn.close()
+  print("Done.")
---
+++ update.py
@@ -0,0 +1,27 @@
+import mysql.connector
+from mysql.connector import errorcode
+from config import config_data
+
+# Construct connection string
+try:
+   conn = mysql.connector.connect(**config_data)
+   print("Connection established")
+except mysql.connector.Error as err:
+  if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+    print("Something is wrong with the user name or password")
+  elif err.errno == errorcode.ER_BAD_DB_ERROR:
+    print("Database does not exist")
+  else:
+    print(err)
+else:
+  cursor = conn.cursor()
+
+  # Update a data row in the table
+  cursor.execute("UPDATE inventory SET quantity = %s WHERE name = %s;", (200, "banana"))
+  print("Updated",cursor.rowcount,"row(s) of data.")
+
+  # Cleanup
+  conn.commit()
+  cursor.close()
+  conn.close()
+  print("Done.")
