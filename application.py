e325c41a1d0bb6ebf0efa150a9fe8937a13caabd
PrzemoNo1 <37351230+PrzemoNo1@users.noreply.github.com>
2020-01-21 19:50:25 +0100

    odpalanie

--- application.py
+++ application.py
@@ -1,6 +1,8 @@
 from flask import Flask
+import create
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
+    create.start_creating()
--- create.py
+++ create.py
@@ -2,38 +2,39 @@ import mysql.connector
 from mysql.connector import errorcode
 from config import config_data
 
-# Construct connection string
-try:
-   conn = mysql.connector.connect(**config_data)
-   print("Connection established")
-except mysql.connector.Error as err:
-  if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-    print("Something is wrong with the user name or password")
-  elif err.errno == errorcode.ER_BAD_DB_ERROR:
-    print("Database does not exist")
-  else:
-    print(err)
-else:
-  cursor = conn.cursor()
+def start_creating():
+    # Construct connection string
+    try:
+    conn = mysql.connector.connect(**config_data)
+    print("Connection established")
+    except mysql.connector.Error as err:
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print("Something is wrong with the user name or password")
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print("Database does not exist")
+    else:
+        print(err)
+    else:
+    cursor = conn.cursor()
 
-  # Drop previous table of same name if one exists
-  cursor.execute("DROP TABLE IF EXISTS inventory;")
-  print("Finished dropping table (if existed).")
+    # Drop previous table of same name if one exists
+    cursor.execute("DROP TABLE IF EXISTS inventory;")
+    print("Finished dropping table (if existed).")
 
-  # Create table
-  cursor.execute("CREATE TABLE inventory (id serial PRIMARY KEY, name VARCHAR(50), quantity INTEGER);")
-  print("Finished creating table.")
+    # Create table
+    cursor.execute("CREATE TABLE inventory (id serial PRIMARY KEY, name VARCHAR(50), quantity INTEGER);")
+    print("Finished creating table.")
 
-  # Insert some data into table
-  cursor.execute("INSERT INTO inventory (name, quantity) VALUES (%s, %s);", ("banana", 150))
-  print("Inserted",cursor.rowcount,"row(s) of data.")
-  cursor.execute("INSERT INTO inventory (name, quantity) VALUES (%s, %s);", ("orange", 154))
-  print("Inserted",cursor.rowcount,"row(s) of data.")
-  cursor.execute("INSERT INTO inventory (name, quantity) VALUES (%s, %s);", ("apple", 100))
-  print("Inserted",cursor.rowcount,"row(s) of data.")
+    # Insert some data into table
+    cursor.execute("INSERT INTO inventory (name, quantity) VALUES (%s, %s);", ("banana", 150))
+    print("Inserted",cursor.rowcount,"row(s) of data.")
+    cursor.execute("INSERT INTO inventory (name, quantity) VALUES (%s, %s);", ("orange", 154))
+    print("Inserted",cursor.rowcount,"row(s) of data.")
+    cursor.execute("INSERT INTO inventory (name, quantity) VALUES (%s, %s);", ("apple", 100))
+    print("Inserted",cursor.rowcount,"row(s) of data.")
 
-  # Cleanup
-  conn.commit()
-  cursor.close()
-  conn.close()
-  print("Done.")
+    # Cleanup
+    conn.commit()
+    cursor.close()
+    conn.close()
+    print("Done.")
