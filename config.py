841783f79b50835f6e96bb75b9b2aa642ac4e9f9
PrzemoNo1 <37351230+PrzemoNo1@users.noreply.github.com>
2020-01-22 08:14:32 +0100

    final version for creating

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ config_data = {
   'host':'server58101.mysql.database.azure.com',
   'user':'myadmin@server58101',
   'password':'DsasD1234',
-  'database':'server58101'
+  'database':'mysampledb'
 }
--- read.py
+++ read.py
@@ -3,30 +3,31 @@ from mysql.connector import errorcode
 from config import config_data
 
 # Construct connection string
-try:
-   conn = mysql.connector.connect(**config_data)
-   print("Connection established")
-except mysql.connector.Error as err:
-  if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-    print("Something is wrong with the user name or password")
-  elif err.errno == errorcode.ER_BAD_DB_ERROR:
-    print("Database does not exist")
-  else:
-    print(err)
-else:
-  cursor = conn.cursor()
+def start_reading():
+    try:
+        conn = mysql.connector.connect(**config_data)
+        print("Connection established")
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("Something is wrong with the user name or password")
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print("Database does not exist")
+        else:
+            print(err)
+    else:
+        cursor = conn.cursor()
 
-  # Read data
-  cursor.execute("SELECT * FROM inventory;")
-  rows = cursor.fetchall()
-  print("Read",cursor.rowcount,"row(s) of data.")
+        # Read data
+        cursor.execute("SELECT * FROM inventory;")
+        rows = cursor.fetchall()
+        print("Read",cursor.rowcount,"row(s) of data.")
 
-  # Print all rows
-  for row in rows:
-    print("Data row = (%s, %s, %s)" %(str(row[0]), str(row[1]), str(row[2])))
+        # Print all rows
+        for row in rows:
+            print("Data row = (%s, %s, %s)" %(str(row[0]), str(row[1]), str(row[2])))
 
-  # Cleanup
-  conn.commit()
-  cursor.close()
-  conn.close()
-  print("Done.")
+        # Cleanup
+        conn.commit()
+        cursor.close()
+        conn.close()
+        print("Done.")
